867f4b6648c51e46e2680fe201cdb2748d5f3162
Travis Jankowski <38842564+TravisJ33304@users.noreply.github.com>
2019-01-08 18:24:04 -0600

    Add files via upload

---
+++ snake/main.js
@@ -0,0 +1,153 @@
+var canvas, ctx;
+
+var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+window.requestAnimationFrame = requestAnimationFrame;
+
+var toRender = [];
+
+var lost = false;
+
+var speed = 500;
+
+var head = {
+    x: 240,
+    y: 360,
+    dir: "d",
+};
+
+var apple = {
+    x: Math.random() * 400 + 25,
+    y: Math.random() * 300 + 25,
+};
+
+var tails = [{
+        x: head.x-30,
+        y: head.y,
+        par: undefined,
+        dir: "d",
+    },{
+        x: head.x-60,
+        y: head.y,
+        par: undefined,
+        dir: "d",
+    },{
+        x: head.x-60,
+        y: head.y-30,
+        par: undefined,
+        dir: "s",
+    },{
+        x: head.x-60,
+        y: head.y-60,
+        par: undefined,
+        dir: "s",
+    },];
+
+tails[0].par = head;
+tails[1].par = tails[0];
+tails[2].par = tails[1];
+tails[3].par = tails[2];
+
+function testCollision(obj, w) {
+    return head.x < obj.x + w &&
+       head.x + 25 > obj.x &&
+       head.y < obj.y + w &&
+       25 + head.y > obj.y;
+}
+
+function restart() {
+    toRender = [];
+    lost = false;
+    speed = 500;
+    head = { x: 240, y: 360, dir: "d",};
+    apple = { x: Math.random() * 400 + 25, y: Math.random() * 300 + 25,};
+    tails = [{ x: head.x-30, y: head.y, par: undefined, dir: "d",},
+    { x: head.x-60, y: head.y, par: undefined, dir: "d",},
+    { x: head.x-60, y: head.y-30, par: undefined, dir: "s",},
+    { x: head.x-60, y: head.y-60, par: undefined, dir: "s",},];
+    tails[0].par = head;
+    tails[1].par = tails[0];
+    tails[2].par = tails[1];
+    tails[3].par = tails[2];
+}
+
+function init() {
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+    ctx.strokeStyle = 'black';
+    window.setInterval(function() {
+        if (!lost) {
+            speed -= 0.5;
+            if (head.dir == "w") {
+                head.y -= 30;
+            } if (head.dir == "a") {
+                head.x -= 30;
+            } if (head.dir == "s") {
+                head.y += 30;
+            } if (head.dir == "d") {
+                head.x += 30;
+            }
+            tails.forEach(function(elem) {
+                if (elem.dir == "w") {
+                    elem.y -= 30;
+                } if (elem.dir == "a") {
+                    elem.x -= 30;
+                } if (elem.dir == "s") {
+                    elem.y += 30;
+                } if (elem.dir == "d") {
+                    elem.x += 30;
+                }
+            });
+            if (testCollision(apple, 15)) {
+                apple.x = Math.random() * 400 + 25;
+                apple.y = Math.random() * 300 + 25;
+                var parent = tails[tails.length-1];
+                if (parent.dir == "w") {
+                    tx = parent.x;
+                    ty = parent.y + 30;
+                } if (parent.dir == "a") {
+                    tx = parent.x + 30;
+                    ty = parent.y;
+                } if (parent.dir == "s") {
+                    tx = parent.x;
+                    ty = parent.y - 30;
+                } if (parent.dir == "d") {
+                    tx = parent.x - 30;
+                    ty = parent.y;
+                }
+                tails.push({x: tx, y: ty, par: parent, dir: parent.dir});
+            }
+            for (var i = tails.length-1; i > 0; i--) {
+                tails[i].dir = tails[i].par.dir;
+            }
+            tails[0].dir = head.dir;
+            if (head.x <= 0) lost = true;
+            if (head.y <= 0) lost = true;
+            if (head.x+25 >= canvas.width) lost = true;
+            if (head.y+25 >= canvas.height) lost = true;
+            for (i = 0; i < tails.length; i++) {
+                if (testCollision(tails[i], 25)) {
+                    lost = true;
+                    break;
+                }
+            }
+        }
+    window.requestAnimationFrame(function() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = 'black';
+        ctx.font = '32px Verdana';
+        ctx.fillText(String(tails.length-4), 15, 40);
+        ctx.fillStyle = 'red';
+        ctx.fillRect(apple.x, apple.y, 15, 15);
+        ctx.fillStyle = 'green';
+        ctx.fillRect(head.x, head.y, 25, 25);
+        ctx.strokeRect(head.x, head.y, 25, 25);
+        ctx.fillStyle = 'rgb(0, 255, 0)';
+        tails.forEach(function(elem) {
+            ctx.fillRect(elem.x, elem.y, 25, 25);
+            ctx.strokeRect(elem.x, elem.y, 25, 25);
+        });
+        ctx.fillStyle = 'black';
+        if (lost) ctx.fillText("You Lost!", canvas.width/2-75, canvas.height/2);
+    });
+    }, speed);
+}
